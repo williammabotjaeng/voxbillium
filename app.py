c40d58f9b85998ac16d26db6552adedcbd91ea28
williammabotjaeng@gmail.com <williammabotjadev@gmail.com>
2023-09-17 09:27:52 +0200

    routes, /contact/what/register/login, app.py

--- app.py
+++ app.py
@@ -4,5 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    
     return render_template("index.html")
+
+@app.route("/login")
+def hello_world():
+    return render_template("login.html")
+
+@app.route("/register")
+def hello_world():
+    return render_template("register.html")
+
+@app.route("/what")
+def hello_world():
+    return render_template("what.html")
+
+@app.route("/contact")
+def hello_world():
+    return render_template("contact.html")
